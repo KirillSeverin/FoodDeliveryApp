f8b9d0c6b733e64a9f85b2aa0dcf8a601fe1c38c
Kirill Severin <kirillseverin2020@gmail.com>
2024-02-21 16:10:50 +0300

    Created base project. Removed storyboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Kirill Severin on 21.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
