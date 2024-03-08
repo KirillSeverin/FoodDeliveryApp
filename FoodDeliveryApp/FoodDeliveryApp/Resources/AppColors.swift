ffe92b926a0ec94ee394857d61fb38aa33f1a5aa
Kirill Severin <kirillseverin2020@gmail.com>
2024-03-09 00:11:56 +0300

    Added new button to onboarding

--- FoodDeliveryApp/FoodDeliveryApp/Resources/AppColors.swift
+++ FoodDeliveryApp/FoodDeliveryApp/Resources/AppColors.swift
@@ -9,7 +9,7 @@ import UIKit
 
 enum AppColors {
     static let accentOrange = UIColor().hex(0xD35400)
-    static let grey = UIColor().hex(0xECF0F1)
+    static let gray = UIColor().hex(0xECF0F1)
     static let black = UIColor().hex(0x000000)
     static let backgroundWhite = UIColor().hex(0xE5E5E5)
     static let white = UIColor.white
